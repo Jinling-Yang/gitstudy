d3132351b3ca0277fafc2118e1607172f051a09d
yang-linda <yang.jinl@northeastern.edu>
2023-01-19 23:59:43 -0500

    test delete

--- src/gitstudy/Happy.java
+++ src/gitstudy/Happy.java
@@ -4,10 +4,10 @@ public class Happy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("a");
+		
 		System.out.println("for test");
 		System.out.println("test 2");
-		System.out.println("test ");
+		
 		int a = 0;
 	}
 
